a0625d22e4ecaf9245496e48ca3788a3b930a188
lupo <bensender@gmail.com>
2022-09-19 15:02:46 +0300

    Replaced callbacks with Promises

--- dogBread/index.js
+++ dogBread/index.js
@@ -1,19 +1,32 @@
 const fs = require('fs');
 const superagent = require('superagent');
 
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+const readFilePromice = file => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(file, (err, data) => {
+            if(err)reject("failed to read the file :(");
+            resolve(data);
+        })
+    })
+}
+
+const writeFilePromice = (file, message) => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(file, message, err => {
+            if(err) reject("failed to write file");
+            resolve("success");
+        });
+    })
+}
+
+readFilePromice(`${__dirname}/dog.txt`).then(data => {
     console.log(`Breed: ${data}`);
-
-    superagent
-        .get(`https://dog.ceo/api/breed/${data}/images/random`)
-        .then(res => {
-            console.log(res.body.message);
-            fs.writeFile('dog-img.txt', res.body.message, err => {
-                console.log('Random dog image saved to the file!');
-            });
-
-    }).catch(err => {
-        console.log(err.message);
-    });
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)
+}).then(res => {
+    console.log(res.body.message);
+    return writeFilePromice('dog-img.txt', res.body.message)
+}).then(() => {
+    console.log('Random dog image saved to the file!');
+}).catch(err => {
+    console.log(err.message);
 });
-
