8de95db5c7c4440b6856a4e2a37fd6947047c95a
lupo <bensender@gmail.com>
2022-09-19 15:42:40 +0300

    Adding await to async functions

--- dogBread/index.js
+++ dogBread/index.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
 const superagent = require('superagent');
 
-const readFilePromice = file => {
+const readFilePromice = fileName => {
     return new Promise((resolve, reject) => {
-        fs.readFile(file, (err, data) => {
+        console.log("About to read the file " + fileName)
+        fs.readFile(fileName, (err, data) => {
             if(err)reject("failed to read the file :(");
             resolve(data);
         })
@@ -19,14 +20,29 @@ const writeFilePromice = (file, message) => {
     })
 }
 
-readFilePromice(`${__dirname}/dog.txt`).then(data => {
-    console.log(`Breed: ${data}`);
-    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)
-}).then(res => {
-    console.log(res.body.message);
-    return writeFilePromice('dog-img.txt', res.body.message)
-}).then(() => {
-    console.log('Random dog image saved to the file!');
-}).catch(err => {
-    console.log(err.message);
-});
+const getDogPic = async () => {
+    try{
+        const data = await readFilePromice(`${__dirname}/dog.txt`);
+        console.log(`Breed: ${data}`);
+        const res = await superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+        console.log(res.body.message);
+        writeFilePromice('dog-img.txt', res.body.message);
+        console.log('Random dog image saved to the file!');
+    }catch(err) {
+        console.log(err);    
+    }
+}
+
+getDogPic();
+
+// readFilePromice(`${__dirname}/dog.txt`).then(data => {
+//     console.log(`Breed: ${data}`);
+//     return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)
+// }).then(res => {
+//     console.log(res.body.message);
+//     return writeFilePromice('dog-img.txt', res.body.message)
+// }).then(() => {
+//     console.log('Random dog image saved to the file!');
+// }).catch(err => {
+//     console.log(err.message);
+// });
